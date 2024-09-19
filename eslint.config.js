d03fd1eed64bb9801cf9aa3978f38b8ba32bbac5
Ian Jennings <ian@meetjennings.com>
2024-09-19 12:57:14 -0500

    fix for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,20 @@ const globals = require("globals");
 const pluginJs = require("@eslint/js");
 
 module.exports = [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.browser } },
-  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
-  { ignorePatterns: ["lib/subimage/opencv.js"] }, // Add this line
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    }
+  },
+  { 
+    // this needs to be it's own object for some reason
+    // https://github.com/eslint/eslint/issues/17400   
+    ignores: ["lib/subimage/opencv.js", "node_modules/**", ".git"],
+  }
 ];
