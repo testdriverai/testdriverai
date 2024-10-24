4e2bad5891af234af03b8d1488c74606c22d4948
Tarun Kumar S <srfashions.tarun@gmail.com>
2024-10-24 21:13:31 +0530

    add summarize config

--- index.js
+++ index.js
@@ -233,7 +233,9 @@ const haveAIResolveError = async (error, markdown, depth = 0, undo = false) => {
   if (errorCounts[safeKey] > errorLimit - 1) {
     console.log(chalk.red("Error loop detected. Exiting."));
     console.log(eMessage);
-    await summarize(eMessage);
+    if (config.TD_SUMMARIZE) {
+      await summarize(eMessage);
+    }
     return await exit(true);
   }
 
@@ -271,7 +273,9 @@ const check = async () => {
 
   if (checkCount >= checkLimit) {
     log.log("info", chalk.red("Exploratory loop detected. Exiting."));
-    await summarize("Check loop detected.");
+    if (config.TD_SUMMARIZE) {
+      await summarize("Check loop detected.");
+    }
     return await exit(true);
   }
 
@@ -317,7 +321,9 @@ const runCommand = async (command, depth) => {
     if (error.fatal) {
       console.log("");
       log.log("info", chalk.red("Fatal Error") + `\n${error.message}`);
-      await summarize(error.message);
+      if (config.TD_SUMMARIZE) {
+        await summarize(error.message);
+      }
       return await exit(true);
     } else {
       return await haveAIResolveError(
@@ -854,8 +860,9 @@ let run = async (file, overwrite = false, shouldExit = true) => {
       console.log(
         `Version mismatch: ${file}. Trying to run a test with v${ymlObj.version} test when this package is v${package.version}.`,
       );
-
-      await summarize("Version mismatch");
+      if (config.TD_SUMMARIZE) {
+        await summarize("Version mismatch");
+      }
       await exit(true);
     }
   }
