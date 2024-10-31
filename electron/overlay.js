c4135a915fa639609f57b16f8824b24b8dae8ed4
Yassine El Khattabi <yassine@maroline.dev>
2024-10-31 13:57:09 +0100

    Refactor and simplify events registration

--- electron/overlay.js
+++ electron/overlay.js
@@ -1,6 +1,6 @@
 const ipc = require("node-ipc").default;
 const { app, screen, BrowserWindow } = require("electron");
-const { events } = require("../lib/events.js");
+const { eventsArray } = require("../lib/events.js");
 
 ipc.config.id = "testdriverai_overlay";
 ipc.config.retry = 1500;
@@ -27,21 +27,11 @@ app.whenReady().then(() => {
   window.loadFile("overlay.html");
 
   ipc.serve(() => {
-    ipc.server.on(events.updateBoundingBoxes, (data) => {
-      window?.webContents.send(events.updateBoundingBoxes, data);
-    });
-    ipc.server.on(events.mouseClick, (data) => {
-      window?.webContents.send(events.mouseClick, data);
-    });
-    ipc.server.on(events.screenCapture.start, (data) => {
-      window?.webContents.send(events.screenCapture.start, data);
-    });
-    ipc.server.on(events.screenCapture.end, (data) => {
-      window?.webContents.send(events.screenCapture.end, data);
-    });
-    ipc.server.on(events.screenCapture.error, (data) => {
-      window?.webContents.send(events.screenCapture.end, data);
-    });
+    for (const event of eventsArray) {
+      ipc.server.on(event, (data) => {
+        window?.webContents.send(event, data);
+      });
+    }
   });
   ipc.server.start();
 });
