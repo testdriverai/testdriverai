e533da2ed7465483eeedc0b379e72f2d11e8c2a0
Ian Jennings <ian@meetjennings.com>
2024-11-06 20:35:09 -0600

    Yassine/close matches bounding boxes (#65)
    * Add overlay wth screenshot and clicks feedback
    
    * Add empty line before streamed markdown
    
    * Use the new API version
    
    * Refactor and simplify events registration
    
    * Display bounding boxes all returned matches
    
    * Display bounding boxes for text as well
    
    * Add overlays and use updated streaming (#61)
    
    * Use content protection
    
    * Refactor entrypoint
    
    * Add ping timeout termination to avoid dangling electron processes
    
    * Intercept all stdio outputs
    
    * Hide the terminal window until the user is prompted again
    
    * Mirror the terminal output into the overlay terminal
    
    * Hide and show the overlay terminal depending on prompt
    
    * update window spawn for mac
    
    * Fix crashes on non mac systems
    
    * Remove content protection to record overlays in dashcam replays
    
    * Update package lock
    
    * Fix typos
    
    * Normalize closeMatches on the server
    
    * Default to turbo
    
    * Fix tmeout interval
    
    * Switch from ping timeout to ipc disconnect event to handle hanginig electron processes
    
    * Major Performance Improvements (#62)
    
    * small bugfix
    
    * major perf improvements
    
    * fix console output
    
    * redraw updates
    
    * remove tree kill
    
    * remove error logging
    
    ---------
    
    Co-authored-by: Yassine <yassine@maroline.dev>
    
    * touch up for mac, improve animations
    
    * Hide electron stdio
    
    * Allow larger than screen on mac
    
    * Fix electron startup on windows
    
    * Remove logged display
    
    * Fix subsequent screenshot overlays
    
    * remove console.log
    
    * Fix taskbar issue on windows
    
    * Make sure subsequent clicks animatio works
    
    * only do fullscreen if not on mac
    
    * it seems like this isn't needed
    
    * change env param to TD_OVERLAY
    
    * eslint fixes
    
    ---------
    
    Co-authored-by: Ian Jennings <ian@meetjennings.com>
    
    ---------
    
    Co-authored-by: Yassine El Khattabi <yassine@maroline.dev>

---
+++ electron/overlay.js
@@ -0,0 +1,58 @@
+const ipc = require("node-ipc").default;
+const { app, screen, BrowserWindow } = require("electron");
+const { eventsArray } = require("../lib/events.js");
+
+ipc.config.id = "testdriverai_overlay";
+ipc.config.retry = 1500;
+ipc.config.silent = true;
+
+app.whenReady().then(() => {
+  app.dock?.hide();
+
+  const windowOptions = {
+    ...screen.getPrimaryDisplay().bounds,
+    enableLargerThanScreen: true,
+    frame: false,
+    show: false,
+    closable: false,
+    resizable: false,
+    focusable: false,
+    fullscreenable: true,
+    transparent: true,
+    alwaysOnTop: true,
+    skipTaskbar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    autoHideMenuBar: true,
+  };
+
+  if (process.platform !== 'darwin') {
+    windowOptions.fullscreen = true;
+  }
+
+  const window = new BrowserWindow(windowOptions);
+  window.setIgnoreMouseEvents(true);
+  window.setAlwaysOnTop(true, "screen-saver");
+  window.setVisibleOnAllWorkspaces(true, {
+    visibleOnFullScreen: true,
+  });
+  window.loadFile("overlay.html");
+  window.show();
+
+  // open developer tools
+  // window.webContents.openDevTools();
+
+  ipc.serve(() => {
+    for (const event of eventsArray) {
+      ipc.server.on(event, (data) => {
+        window?.webContents.send(event, data);
+      });
+    }
+  });
+  ipc.server.on("socket.disconnected", function () {
+    process.exit();
+  });
+  ipc.server.start();
+});
