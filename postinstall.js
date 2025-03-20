7d45c67fd04d5783e86c37406690d0f02549366f
Ian Jennings <ian@meetjennings.com>
2025-03-20 16:27:05 -0500

    small mousepos fix

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,7 @@
 let platform = require("os").platform();
 let exec = require("child_process").exec;
+const { execSync } = require("child_process");
+const readline = require("readline");
 
 if (platform !== "darwin") {
   console.log("TestDriver Setup: Skipping codesign becasue not on Mac");
@@ -17,4 +19,26 @@ exec(signScript, (error, stdout, stderr) => {
   }
   console.log(`stdout: ${stdout}`);
   console.error(`stderr: ${stderr}`);
+});
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Do you want to install Vue support? (y/n) ", (answer) => {
+  if (answer.toLowerCase() === "y") {
+    console.log("Installing Vue dependencies...");
+    execSync("npm install -g vue vue-router", { stdio: "inherit" });
+  }
+
+  rl.question("Do you want to install React support? (y/n) ", (answer2) => {
+    if (answer2.toLowerCase() === "y") {
+      console.log("Installing React dependencies...");
+      execSync("npm install -g react react-dom", { stdio: "inherit" });
+    }
+
+    console.log("Setup complete!");
+    rl.close();
+  });
 });
