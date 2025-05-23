bb947cd724e81557e4d06ef34938e44fc966485a
Ian Jennings <ian@meetjennings.com>
2025-05-23 12:50:39 -0500

    remove logs

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 #!/usr/bin/env node
-
-// being called on server with 
-// {
-//  TD_SPEAK: false,
-//  TD_ANALYTICS: true,
-//  TD_NOTIFY: false,
-//  TD_MINIMIZE: false,
-//  TD_API_ROOT: 'https://api.testdriver.ai',
-//  TD_API_KEY: 'xxx',
-//  TD_DEV: undefined,
-//  TD_PROFILE: false,
-//  TD_OVERLAY: true,
-//  TD_SECRET: 'xxx',
-//  TD_VM: true,
-//  TD_OVERLAY_ID: null,
-//  TD_VM_RESOLUTION: [ 1920, 1080 ],
-//  TD_IPC_ID: 'testdriverai_4708',
-//  TD_INTERPOLATION_VARS: '{"TD_WEBSITE":"https://testdriver-sandbox.vercel.app","TD_VM_RESOLUTION":"1920x1080"}',
-//  TD_WEBSITE: 'https://testdriver-sandbox.vercel.app'
-// }
-
 const config = require("./lib/config.js");
 
 // We need to initialize the IPC server quickly
 require("./lib/ipc.js");
 const { emitter, events } = require("./lib/events.js");
 const { logger } = require("./lib/logger.js");
-
-logger.info('Config is')
-console.log(config)
 
 if (process.argv[2] === "--help" || process.argv[2] === "-h") {
   console.log("Command: testdriverai [init, run, edit] [yaml filepath]");
