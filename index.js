918b68484339931794d7d2cc8b2b1b72a683db80
Ian Jennings <ian@meetjennings.com>
2025-07-06 18:33:40 -0500

    chrome example

--- index.js
+++ index.js
@@ -2,15 +2,10 @@
 const os = require("os");
 
 const TestDriverAgent = require("./agent.js");
-
-// Create an instance of the agent
 const agent = new TestDriverAgent();
 
-// Get the current process ID
-const pid = process.pid;
-
 try {
-  // Set the priority to the highest value
+  const pid = process.pid;
   os.setPriority(pid, -20);
   // eslint-disable-next-line no-unused-vars
 } catch (error) {
