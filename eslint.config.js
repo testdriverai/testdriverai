d3969867e73f36653e5c849324d828b67855482c
Ian Jennings <ian@meetjennings.com>
2025-07-14 17:50:02 -0500

    eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ const pluginJs = require("@eslint/js");
 module.exports = [
   pluginJs.configs.recommended,
   {
-    files: ["**/*.js"],
+    files: ["./interfaces/**/*.js", "./agent/**/*.js"],
     languageOptions: {
       sourceType: "commonjs",
       globals: {
@@ -17,11 +17,6 @@ module.exports = [
   {
     // this needs to be it's own object for some reason
     // https://github.com/eslint/eslint/issues/17400
-    ignores: [
-      "lib/subimage/opencv.js",
-      "node_modules/**",
-      ".git",
-      "electron/terminal/xterm*",
-    ],
+    ignores: ["agent/lib/subimage/**", "node_modules/**", ".git"],
   },
 ];
