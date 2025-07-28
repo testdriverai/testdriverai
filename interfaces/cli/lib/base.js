9734d7195a9adb17ca80b024f92bdf796aaabc0a
Tarun Kumar S <tarun@testdriver.ai>
2025-07-28 21:45:50 +0530

    update live url for CI (#372)
    * update live url for CI
    
    * use searchParams
    
    ---------
    
    Co-authored-by: Eric Clemmons <eric@smarterspam.com>

--- interfaces/cli/lib/base.js
+++ interfaces/cli/lib/base.js
@@ -118,8 +118,14 @@ class BaseCommand extends Command {
     // Handle show window events
     this.agent.emitter.on("show-window", async (url) => {
       console.log(`Live test execution: `);
-      console.log(url);
-      await openBrowser(url);
+      if (this.agent.config.CI) {
+        let u = new URL(url);
+        u = JSON.parse(u.searchParams.get("data"));
+        console.log(`${u.url}?view_only=true`);
+      } else {
+        console.log(url);
+        await openBrowser(url);
+      }
     });
   }
 
