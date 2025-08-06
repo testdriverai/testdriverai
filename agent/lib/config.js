5e4f90546013a1a163da2ec3ce96e825fe219483
Eric Clemmons <eric@smarterspam.com>
2025-08-05 21:06:17 -0500

    Fix TD_API_ROOT

--- agent/lib/config.js
+++ agent/lib/config.js
@@ -21,7 +21,7 @@ const createConfig = (environment = {}) => {
   // Start with defaults
   const config = {
     TD_ANALYTICS: true,
-    TD_API_ROOT: "https://testdriverai-v6-c96fc597be11.herokuapp.com",
+    TD_API_ROOT: "https://v6.testdriver.ai",
     TD_API_KEY: null,
     TD_PROFILE: false,
     TD_RESOLUTION: [1366, 768],
