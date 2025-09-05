42971ea86bbb6e4bdf89907754b9a1ce5bf15682
Tarun Kumar <srfashions.tarun@gmail.com>
2025-09-05 22:16:06 +0530

    fix lint, man everytime

--- agent/lib/commander.js
+++ agent/lib/commander.js
@@ -134,7 +134,7 @@ commands:
             object.description,
             object.action,
             object.method,
-            object.timeout
+            object.timeout,
           );
           break;
         case "hover-image":
